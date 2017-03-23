6dbf00faa61381fc7cfa90e2c4faf4324bfe4ebc
PatMulvihill <mulvihillp993@yahoo.com>
2017-03-22 20:35:49 -0400

    finished matrix_route_calculate for perfect square matrices, have to adjust it so it works for m*n matrices

--- matrix_route_calculator/matrix_route_calculator.py
+++ matrix_route_calculator/matrix_route_calculator.py
@@ -9,8 +9,8 @@
 
 import random
 import numpy as np
-w = 3
-h = 3
+w = 10
+h = 10
 # declare grid variable which represents our matrix
 grid = [[0 for x in range(w)] for y in range(h)]
 # create a routes matrix of the same size as grid that will store the amount
@@ -21,7 +21,7 @@ for i in range(w):
         # initalise grid matrix with random values (which don't matter in this problem)
         grid[i][j] = random.randint(0, 5)
         # initialise routes matrix with values of 0 so that we can effectively index in
-        routes[i][j] = -1
+        routes[i][j] = 0
 print("Starting Grid:")
 print(np.matrix(grid))
 print("\nRoutes Matrix:")
@@ -33,5 +33,10 @@ print(np.matrix(routes))
 for i in range(w):
     for j in range(h):
         if i==0 or j == 0:
-            costs[i][j] = 0
+            routes[i][j] = 1
+        else:
+            routes[i][j] = routes[i - 1][j] + routes[i][j-1]
+print("\nOnce all the routes have been calculated, our routes matrix looks like this.")
 print(np.matrix(routes))
+print("\nThe total number of routes will be contained in the [w-1][h-1] index.")
+print("The total number of routes possible is", routes[w - 1][h - 1])
